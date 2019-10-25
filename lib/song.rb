01e8450d990a7f6b1f046d4f65acd50236d6f59f
Victoria Hale <vic.hale74@gmail.com>
2019-10-25 18:24:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,25 +11,18 @@ class Song
     @@songs << self
   end
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+  # def self.find_by_name(name)
+  #   @@songs.detect{|a| a.name == name}
+  # end
 
   def self.all
     @@songs
   end
 
-  # def self.reset_all
-  #   self.all.clear
-  # end
 
-  def self.count
-    self.all.count
+  def artist=(artist)
+    @artist = artist
   end
-
-  # def artist=(artist)
-  #   @artist = artist
-  # end
 
   def to_param
     name.downcase.gsub(' ', '-')
