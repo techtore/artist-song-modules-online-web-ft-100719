392bfff68fab4c3e890668c242ee93777c0941e5
Victoria Hale <vic.hale74@gmail.com>
2019-10-25 18:02:24 +0000

    Automatically backed up by Learn

--- concerns/memorable.rb
+++ concerns/memorable.rb
@@ -1,10 +1,10 @@
 
 module Memorable
-  # def self.reset_all
-  #   self.all.clear
-  # end
+  def self.reset_all
+    self.all.clear
+  end
 
-  # def self.count
-  #   self.all.count
-  # end
+  def self.count
+    self.all.count
+  end
 end
